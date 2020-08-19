130b357298019eaa29065a0a5c40b3bb56a01eaa
kevin <kevin.thea@gmail.com>
2020-08-19 19:11:05 -0400

    update

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello")
+# Takes in user first name and last name and prints it out to the console.
+# No error handling yet.
+
+first_name = input("Enter your first name ")
+last_name = input("Enter your last name: ")
+
+print("\nFirst name: " + first_name + " \n" + "Last name: " + last_name)
